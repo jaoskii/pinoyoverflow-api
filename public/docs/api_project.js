b0c210cfcbb818de17e038392ae7f1016dac50ab
jaoski <tamlaylovestokwa@gmail.com>
2021-04-29 18:14:11 +0800

    added apiDoc plugin

---
+++ public/docs/api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "Pinoy Overflow API",
+  "version": "0.1.0",
+  "title": "Pinoy Overflow API",
+  "url": "https://api.github.com/api",
+  "useHostUrlAsSampleUrl": true,
+  "description": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-04-29T10:05:51.539Z",
+    "url": "https://apidocjs.com",
+    "version": "0.27.1"
+  }
+});
--- routes/v1/auth.js
+++ routes/v1/auth.js
@@ -17,6 +17,34 @@ const reqhandler = new requestHandlers();
 const jwtman = new jwtManager();
 const upload = multer()
 
+
+/**
+ * @api {get} /user/:id Get User information
+ * @apiVersion 0.1.0
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
+
 //LOGIN
 router.post('/login', upload.none(), function(req, res, next) {
   // generate salt to hash password
